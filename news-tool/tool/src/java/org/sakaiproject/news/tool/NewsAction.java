9a493e5beb75c215df59d4754240420110e9efdd
steve.swinsburg@gmail.com <steve.swinsburg@gmail.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2011-11-10 01:32:44 +0000

    SAK-20986 merge trunk r97330
    git-svn-id: https://source.sakaiproject.org/svn/web/branches/sakai-2.8.x@100620 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- news-tool/tool/src/java/org/sakaiproject/news/tool/NewsAction.java
+++ news-tool/tool/src/java/org/sakaiproject/news/tool/NewsAction.java
@@ -24,6 +24,7 @@ package org.sakaiproject.news.tool;
 import java.net.URL;
 import java.util.List;
 import java.util.Vector;
+import java.text.DateFormat;
 
 import org.sakaiproject.cheftool.Context;
 import org.sakaiproject.cheftool.JetspeedRunData;
@@ -50,7 +51,10 @@ import org.sakaiproject.tool.api.ToolSession;
 import org.sakaiproject.tool.cover.SessionManager;
 import org.sakaiproject.tool.cover.ToolManager;
 import org.sakaiproject.util.ResourceLoader;
+import org.sakaiproject.util.FormattedText;
+import org.sakaiproject.util.Validator;
 import org.apache.commons.lang.StringUtils;
+import org.sakaiproject.time.cover.TimeService;
 
 /**
  * <p>
@@ -195,7 +199,9 @@ public class NewsAction extends VelocityPortletPaneledAction
 
 		context.put("channel", channel);
 		context.put("news_items", items);
- 
+		DateFormat df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT, new ResourceLoader().getLocale());	
+		df.setTimeZone(TimeService.getLocalTimeZone());
+		context.put("dateFormat", df);
 		try 
 		{
 			EventTrackingService.post(EventTrackingService.newEvent(FEED_ACCESS, "/news/site/" +
